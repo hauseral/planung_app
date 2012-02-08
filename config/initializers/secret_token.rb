a3a32cb7f813d84f02c990c84d8469c1ef99c1a8
Alexandra Hauser <alexandra_ha@freenet.de>
2012-02-08 19:11:08 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanungApp::Application.config.secret_token = 'b61da6b04c6bf5d87db615c939d0786942f756aaa49e52fb88c6410539e52a44ed0cd9896cb8b88e36d67219f59c7bcc36f0b0555ef7f5f47154790e9224393c'
